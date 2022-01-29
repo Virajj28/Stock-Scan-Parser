80005f67dbea82ac77e7c18d7a0c2dc85cdf588b
Virajj28 <viraj28j@gmail.com>
2022-01-29 13:11:01 +0530

    update UI replica

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://mobile-app-challenge.herokuapp.com/data')
+    .then(res => res.json())
+    .then(data => {
+      setData(data);
+    })
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello World !</h1>
+        {data.map(item => (
+          <div key={item.id} className='card'>
+            <div className='card-item' >
+              <div className='card-item-header'>
+                {item.name}
+              </div>
+              <div
+                style={{color: item.color}}
+                className='card-item-body'
+              >
+                {item.tag}
+              </div>
+            </div>
+          </div>
+        ))}
     </div>
   );
 }
