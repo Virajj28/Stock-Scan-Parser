cdd3bf493c082058eddbfd54b07a9d693f9ee746
Virajj28 <viraj28j@gmail.com>
2022-01-29 23:49:08 +0530

    criteria hide & show

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import './App.css';
 import { useEffect, useState } from 'react';
+import Data from './components/Data';
+import Criteria from './components/Criteria';
 
 function App() {
 
   const [data, setData] = useState([]);
+  const [criteria, setCriteria] = useState(null);
 
   useEffect(() => {
     fetch('https://mobile-app-challenge.herokuapp.com/data')
     .then(res => res.json())
     .then(data => {
+      console.log(data);
       setData(data);
+      // setCriteria(data[0].criteria[0].text);
     })
   }, []);
 
   return (
     <div className="App">
-        {data.map(item => (
-          <div key={item.id} className='card'>
-            <div className='card-item' >
-              <div className='card-item-header'>
-                {item.name}
-              </div>
-              <div
-                style={{color: item.color}}
-                className='card-item-body'
-              >
-                {item.tag}
-              </div>
-            </div>
-          </div>
-        ))}
+      {criteria == null ? <Data data={data} setCriteria={setCriteria}/> : <Criteria criteria={criteria}/> }
     </div>
   );
 }
