de66d58a91d5ee43d3a1163d4f3a241ebc822411
Virajj28 <viraj28j@gmail.com>
2022-01-30 17:21:16 +0530

    update criteria

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import './App.css';
+import axios from 'axios';
 import { useEffect, useState } from 'react';
-import Data from './components/Data';
-import Criteria from './components/Criteria';
 
 function App() {
+    const [data, setData] = useState([]);
+    const [criteria, setCriteria] = useState([]);
+    const [condition, setCondition] = useState(false);
 
-  const [data, setData] = useState([]);
-  const [criteria, setCriteria] = useState(null);
+    const getData =async() => {
+        const response = await axios.get('https://mobile-app-challenge.herokuapp.com/data')
+        const id = []
+        response.data.map((res)=>{
+            id.push(res.id)
+        })
+        getAllData(id,response.data)
+        setData(response.data)
+        }
+    const getAllData =(newID,datas) => {
+        console.log('datas', datas);
+        const alldata = []
+        for(let id of newID){
+           alldata.push(datas.filter((temp)=>(temp.id == id)))
+        }
+        console.log('alldata', alldata);
+        setCriteria(alldata)
 
-  useEffect(() => {
-    fetch('https://mobile-app-challenge.herokuapp.com/data')
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      setData(data);
-      // setCriteria(data[0].criteria[0].text);
-    })
-  }, []);
+    }
+    const handleClick =(id) => {
+        const newID = [id]
+        getAllData(newID,data)
+        setCondition(true)
+    }
+
+    useEffect(()=>{
+        getData()
+    }, [])
 
   return (
-    <div className="App">
-      {criteria == null ? <Data data={data} setCriteria={setCriteria}/> : <Criteria criteria={criteria}/> }
+    <div className='App'>
+        {
+            criteria.map((res)=>(
+                res.map((temp)=>(
+                <div className='card-item' key={temp.id} style={{cursor:'pointer'}} onClick={()=>handleClick(temp.id)}>
+                    <div className='card-item-header'>
+                        {temp.name}
+                    </div>
+                    <div className='card-item-body' style={{color:`${temp.color}`}}>
+                            {temp.tag}
+                    </div>
+                </div>
+            ))))
+        }
+
+        {condition &&
+            criteria.map((tempa)=>
+                { 
+                    return tempa.map((res)=>(
+                    res.criteria.map((temp)=>(
+                        <div className='card-criteria' key={temp.id}>
+                        <tr>
+                            <td>
+                                {temp.text}
+                            </td>
+                        </tr>
+                      </div>
+                    ))
+                ))
+            })
+        }
+
+        <button onClick={()=> setCondition(false)}>Back</button>
     </div>
   );
 }
