67f34b369ae28fd09ab248affa8ca19c466625af
Virajj28 <viraj28j@gmail.com>
2022-01-31 00:06:41 +0530

    add replace method

--- src/components/Criteria.js
+++ src/components/Criteria.js
@@ -3,12 +3,20 @@ const Criteria = (condition,criteria) => {
         <>
             {
                 condition &&
-                criteria.map(sub=>
+                criteria.map((sub,i) =>
                     { 
                         return sub.map(res=>(
                         res.criteria.map(temp=>(
-                            <div className='card-criteria' key={temp.id}>
-                            {temp.text} 
+                            <div className='card-criteria' key={sub[i].id}>
+                                {
+                                    sub[i].id === 1? temp.text:
+                                    sub[i].id === 2? temp.text:
+                                    sub[i].id === 3? temp.text.replace('$1',temp.variable['$1'].values[0]):
+                                    sub[i].id === 4? temp.text.replace('$1',temp.variable['$1'].default_value)
+                                        .replace('$2',temp.variable['$2'].values[0]):
+                                    sub[i].id === 5? temp.text:
+                                    "no data"
+                                }
                             </div>
                         ))))
                     }
